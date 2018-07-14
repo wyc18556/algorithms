c8c38233ed535c0f70f03a16feff57d82ef5c320
wyc <wangyechao@sunline.cn>
2018-07-14 11:11:09 +0800

    简单的字符串翻转操作

---
+++ src/OverturnString.java
@@ -0,0 +1,42 @@
+/**
+ * 翻转字符串算法实现
+ * 如：请把“I am a student.”，翻转成“I ma a .tneduts”.
+ */
+public class OverturnString {
+
+    /**
+     *翻转字符串操作
+     *
+     * @param str 待翻转字符串
+     * @return 翻转后的字符串
+     */
+    public static String doThis(String str){
+        char[] chars = str.toCharArray();
+        int start = 0;
+        int end = 0;
+        while (end <= str.length() - 1){
+            //遇到空格，翻转单词
+            if (chars[end] == ' '){
+                reverseCharArray(chars, start, end - 1);
+                start = end + 1;
+            }
+            //翻转最后一个单词，因为最后一个单词没有空格
+            if (end == str.length() - 1){
+                reverseCharArray(chars,start,end);
+            }
+            end ++;
+        }
+        return new String(chars);
+    }
+
+    private static void reverseCharArray(char[] chars, int start, int end){
+        //首尾交换
+       while (start < end){
+           char t = chars[start];
+           chars[start] = chars[end];
+           chars[end] = t;
+           start ++;
+           end --;
+       }
+    }
+}
