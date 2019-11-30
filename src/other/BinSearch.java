ce08e62c94189a403c89f76b3bcacc9dbecef7df
wyc1856 <wyc1856@gmail.com>
2019-11-30 16:16:35 +0800

    修改变量名

--- src/other/BinSearch.java
+++ src/other/BinSearch.java
@@ -17,7 +17,7 @@ public class BinSearch {
     /**
      * 循环方式实现
      */
-    private int binSearch1(int[] array, int a){
+    private int binSearch1(int[] array, int target){
         int start = 0;
         int end = array.length - 1;
         int mid;
@@ -25,9 +25,9 @@ public class BinSearch {
         while (start <= end){
             //注意2：当start和end较大时，相加的和会溢出，所以不要采用 mid = (start + end) >> 1的方式求中间节点。
             mid = start + ((end - start) >> 1);
-            if (array[mid] == a){
+            if (array[mid] == target){
                 return mid;
-            }else if (array[mid] < a){
+            }else if (array[mid] < target){
                 //注意3：当a再右半区间时，下一个开始节点是mid + 1，不是mid，如果区mid可能导致死循环。
                 start = mid + 1;
             }else {
@@ -41,21 +41,21 @@ public class BinSearch {
     /**
      * 递归方式实现
      */
-    private int binSearch2(int[] array, int a){
-        return recursiveBinSearch(array, a, 0, array.length - 1);
+    private int binSearch2(int[] array, int target){
+        return recursiveBinSearch(array, target, 0, array.length - 1);
     }
 
-    private int recursiveBinSearch(int[] array, int a, int start, int end){
+    private int recursiveBinSearch(int[] array, int target, int start, int end){
         if (start > end){
             return -1;
         }
         int mid = start + ((end - start) >> 1);
-        if (array[mid] == a){
+        if (array[mid] == target){
             return mid;
-        }else if (array[mid] < a){
-            return recursiveBinSearch(array, a, mid + 1, end);
+        }else if (array[mid] < target){
+            return recursiveBinSearch(array, target, mid + 1, end);
         }else {
-            return recursiveBinSearch(array, a, start, end -1);
+            return recursiveBinSearch(array, target, start, end -1);
         }
     }
 }
